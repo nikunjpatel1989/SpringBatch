08365d3457e3ae2344f8b7a1b1cbcb65a9cafe90
Nikunj Patel <nikunjpatel1989@yahoo.com>
2018-04-28 23:40:28 +0530

    my first code commit

--- SpringBatchCsvToXml/src/main/java/com/nikunj/springbatch/LocalDateAdapter.java
+++ SpringBatchCsvToXml/src/main/java/com/nikunj/springbatch/LocalDateAdapter.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 import org.joda.time.LocalDate;
 
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate>{
-
+//few code here as wll
 	public LocalDate unmarshal(String v) throws Exception {
 		return new LocalDate(v);
 	}
--- SpringBatchCsvToXml/src/main/java/com/nikunj/springbatch/Main.java
+++ SpringBatchCsvToXml/src/main/java/com/nikunj/springbatch/Main.java
@@ -9,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-
+//few lines of code
 	@SuppressWarnings("resource")
 	public static void main(String areg[]){
 		
